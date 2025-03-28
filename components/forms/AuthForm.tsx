59c77abb8f32427243d4e6bcbfafebbbdda20cbf
okasi <sithuliso@oceanoftechsa.com>
2025-03-29 01:53:14 +0200

    deployment fix

--- components/forms/AuthForm.tsx
+++ components/forms/AuthForm.tsx
@@ -19,7 +19,7 @@ interface AuthProps {
 const AuthForm: React.FC<AuthProps> = ({hideEmailField = false, BtnIcon, action = "login", variant = "login"}) => {
     const [loading, setLoading] = useState(false);
 
-    const handleSubmit = async (formData: any) => {
+    const handleSubmit = async (formData: FormData) => {
         setLoading(true);
         await login(formData);
     }
